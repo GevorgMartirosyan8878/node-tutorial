47cd4c382b6efc1e43cce7422af500cbb2f49684
Gevorg Martirosyan <gevorg.martirosyan.1998.08@gmail.com>
2021-07-09 23:52:51 +0400

    'node-lessons'

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+fs.readFile(__filename, () => {
+    setTimeout(() => {
+        console.log('Timeout') // fourth
+    })
+    console.log('log') // first
+    setImmediate(() => {
+        console.log('immediate') // third
+    })
+    process.nextTick(() => {
+        console.log('Tick') // second
+    })
+})
